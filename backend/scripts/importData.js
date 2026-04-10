18969207479215daa07dcb3045a92dfad5f0c0ff
AyushMishraFeb2005 <ayushmishra9449@gmail.com>
2026-04-11 01:42:42 +0530

    feat: import 500 NGO employee records into MongoDB from CSV with diverse skills and categories

---
+++ backend/scripts/importData.js
@@ -0,0 +1,50 @@
+import fs from "fs";
+import csv from "csv-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import NgoEmployee from "../models/ngoEmployee.models.js";
+
+dotenv.config();
+
+await mongoose.connect(process.env.MONGODB_URL);
+console.log("MongoDB Connected");
+
+const results = [];
+
+fs.createReadStream("./data/ngo_employees.csv") 
+    .pipe(csv())
+    .on("data", (row) => {
+
+        let gender = row.gender === "M" ? "Male" : "Female";
+
+        const skills = row.skills.split(",").map(s => s.trim());
+
+        const category = row.category.split(",").map(c => c.trim());
+
+        const employee = {
+            empId: row.empId,
+            name: row.name,
+            age: Number(row.age),
+            gender,
+            phone: row.phone,
+            skills,
+            category,
+            location: {
+                latitude: Number(row.latitude),
+                longitude: Number(row.longitude),
+            },
+            availability: row.availability,
+        };
+
+        results.push(employee);
+    })
+    .on("end", async () => {
+        try {
+            await NgoEmployee.insertMany(results);
+            console.log("Importing data successful.");
+            process.exit();
+        } catch (error) {
+            console.error("Error during inserting data", error);
+            process.exit(1);
+        }
+    });
